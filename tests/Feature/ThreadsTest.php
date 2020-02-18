7e951636fdc905edbd767241f2c55aab9e8cc463
LynneKinyua <lynnemkin@gmail.com>
2020-02-18 22:52:32 +0300

    lesson 2

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+//use  Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ThreadsTest extends TestCase
