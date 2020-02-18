a9fcb1e4a5c408b0914ad8875c4c00b4f9449cab
LynneKinyua <lynnemkin@gmail.com>
2020-02-19 01:00:55 +0300

    lesson 4

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
  
+@extends('layouts.app')
+ 
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{ $thread->title }}</div>
- 
+                    <div class="panel-heading">
+                        <a href="#">{{ $thread->creator->name }}</a> posted:
+                        {{ $thread->title }}
+                    </div>
                     <div class="panel-body">
                         {{ $thread->body }}
                     </div>
@@ -17,11 +21,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 @foreach($thread->replies as $reply)
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                        {{$reply->owner->name}} said {{ $reply->created_at->diffForHumans() }}
-                        </div>
-                    </div>
+                    @include('threads.reply');
                 @endforeach
             </div>
         </div>
