5c6ac046ae3e98e224627bd675bd3de2eea63c45
LynneKinyua <lynnemkin@gmail.com>
2020-02-19 20:51:59 +0300

    lesson 6

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
+    <body style=”padding-bottom: 100px”>.
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
