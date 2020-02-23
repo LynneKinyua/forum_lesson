fd5b187ecfe8730c642e85f81b3b1067c2e1035c
LynneKinyua <lynnemkin@gmail.com>
2020-02-23 20:17:55 +0300

    lesson 12

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -11,8 +11,13 @@ class ThreadsController extends Controller {
         $this->middleware('auth')->except(['index', 'show']);
 	}
 	
-	public function index() {
-		$threads = Thread::latest()->get();
+	public function index(Channel $channel)
+    {
+        if ($channel->exists) {
+            $threads = $channel->threads()->latest()->get();
+        } else {
+            $threads = Thread::latest()->get();
+        }
 		
 		return view( 'threads.index', compact( 'threads' ) );
 	}
