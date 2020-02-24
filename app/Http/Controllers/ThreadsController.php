40e25818e208953b060498bc0dd58ba2a0f068b6
LynneKinyua <lynnemkin@gmail.com>
2020-02-24 21:19:44 +0300

    lesson 15

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -12,15 +12,22 @@ class ThreadsController extends Controller {
 	}
 	
 	public function index(Channel $channel)
-    {
-        if ($channel->exists) {
-            $threads = $channel->threads()->latest()->get();
-        } else {
-            $threads = Thread::latest()->get();
-        }
-		
-		return view( 'threads.index', compact( 'threads' ) );
-	}
+{
+    if ($channel->exists) {
+        $threads = $channel->threads()->latest();
+    } else {
+        $threads = Thread::latest();
+    }
+ 
+    if ($username = request('by')) {
+        $user = \App\User::where('name', $username)->firstOrFail();
+        $threads->where('user_id', $user->id);
+    }
+ 
+    $threads = $threads->get();
+ 
+    return view('threads.index', compact('threads'));
+}
 	
 	public function create()
     {
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \View::share('channels', \App\Channel::all());
+        \View::composer('*', function ($view) {
+            $view->with('channels', Channel::all());
+        });
     }
  
     /**
