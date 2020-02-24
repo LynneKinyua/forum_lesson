1b4813c14a87772e5bb59d08f0a8ae5359dc1aaf
LynneKinyua <lynnemkin@gmail.com>
2020-02-24 20:59:13 +0300

    lesson 14

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,27 +1,28 @@
 <?php
-
+ 
 namespace App\Providers;
-
+ 
+use function foo\func;
 use Illuminate\Support\ServiceProvider;
-
+ 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
+    {
+        \View::share('channels', \App\Channel::all());
+    }
+ 
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
     {
         //
     }
